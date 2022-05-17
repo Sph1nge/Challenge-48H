eee4c0124d867b16de328ad088e4edcc0731fad3
Nasheurax <105709402+Nasheurax@users.noreply.github.com>
2022-05-17 15:28:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if (username == "admin" && password == "user") {
-        alert("login succesfully");
+        alert("login atrersuccesfully");
         window.location.href='home.html';
         return false;
     }
